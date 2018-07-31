749fd978e8a9630d406b3885832c9e5812aee252
Katharine Francis <katie-francis@hotmail.com>
2018-07-31 15:27:37 -0400

    ls started

--- bash.js
+++ bash.js
@@ -1,15 +1,20 @@
 //Output a prompt
-process.stdout.write('prompt > ');
+const pwd = require("./pwd");
+const ls = require("./ls");
+
+process.stdout.write("prompt > ");
 
 //The stdin 'data' event fires after a user types in a line
-process.stdin.on('data', (data) => {
+process.stdin.on("data", data => {
   const cmd = data.toString().trim(); //remove the new line
 
-  if(cmd === 'pwd') {
-    process.stdout.write(process.cwd());
+  if (cmd === "pwd") {
+    pwd();
+    //process.stdout.write(process.cwd());
+  } else if (cmd === "ls") {
+    ls();
   } else {
-    process.stdout.write('You typed: ' + cmd);
-    process.stdout.write('\nprompt > ');
+    process.stdout.write("You typed: " + cmd);
+    process.stdout.write("\nprompt > ");
   }
-
 });
---
+++ ls.js
@@ -0,0 +1,3 @@
+module.exports = ls;
+
+function ls() {}
